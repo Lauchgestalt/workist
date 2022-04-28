097a18d2cc8707789604d3a6472109371325c1a4
Lauchgestalt <52470042+Lauchgestalt@users.noreply.github.com>
2022-04-28 20:32:02 +0200

    Add files via upload

---
+++ Correction Checkmarks.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         Correction Checkmarks
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://wb.workist.com/job/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=workist.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var corrections;
+
+    function init(){
+        corrections = document.querySelectorAll('.correction');
+        if (corrections.length > 0) {
+            for(let i = 0; i < corrections.length; i++){
+                let checkmark = document.createElement('INPUT');
+                checkmark.setAttribute("type", "checkbox");
+                checkmark.addEventListener('click', function(){event.stopPropagation();})
+                corrections[i].prepend(checkmark);
+            }
+
+            let checkBtn = document.createElement('button');
+            let div = document.querySelector('.word-annotator__corrections');
+            checkBtn.innerHTML = "Copy Results";
+            checkBtn.addEventListener('click', function(){copyResults();})
+            div.append(checkBtn);
+
+            //setButton();
+        } else { setTimeout(init, 0);}
+    }
+    /**function setButton(){
+        var xpath = "//button[text()='Abschließen']";
+        var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        //matchingElement.addEventListener('click', function(){
+
+        //});
+    }**/
+
+    function copyResults(){
+        var results = [];
+        var resultString = "";
+        var jobID = document.location.href.substr(document.location.href.lastIndexOf('/') + 1);
+        for(let i = 0; i < corrections.length; i++){
+            let checkbox = corrections[i].firstChild;
+            if(checkbox.checked){
+                results.push(jobID + ", " + corrections[i].children[1].innerHTML.replace(/\s/g, ''));
+            }
+        }
+        for(let i = 0; i < results.length; i++){
+            resultString += results[i] + "\n";
+        }
+
+        navigator.clipboard.writeText(resultString);
+        alert("Copied results to Clipboard. Paste into Google Sheet, select dropdown, split into columns");
+    }
+
+    init();
+})();
